2c6861740a79ab809a295476899476fcaaec89fe
Abhishek <abhishekkanichery@gmail.com>
2025-05-17 12:30:10 +0530

    setup: added graphql server and other middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
 import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+async function init() {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Express server is running!');
-});
+    // Middleware
+    app.use(express.json());
+    const corsOptions = {
+        origin: '*', // Allow all origins
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: false,
+    };
+    app.use(cors(corsOptions));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+    // Create graphql server
+    const gqlServer = new ApolloServer({
+        typeDefs: "",
+        resolvers: {},
+    })
+
+    // Start the gql server
+    await gqlServer.start();
+
+    app.get('/', (req, res) => {
+        res.send('Express server is running!');
+    });
+
+    app.use('/graphql', expressMiddleware(gqlServer) as express.Express);
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+    });
+}
+
+init().catch((err) => {
+    console.error('Error starting server:', err);
 });
