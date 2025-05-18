9430d8922da25af64dce83a8948eda2c39581a3c
Abhishek <abhishekkanichery@gmail.com>
2025-05-18 10:08:40 +0530

    feat: create user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,24 @@
 import { Logger } from "winston";
 import { createUserPayload } from "../schemas/interfaces/user.interface";
-
+import { createHmac, randomBytes } from "node:crypto";
+import { prisma } from "../lib/db";
 class UserService {
     constructor(private logger: Logger) { }
 
-    public createUser(payload: createUserPayload) {
+    public async createUser(payload: createUserPayload) {
         try {
+            const { firstName, lastName, email, password } = payload;
+            const salt = randomBytes(32).toString();
+            const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
+            return await prisma.user.create({
+                data: {
+                    email: email,
+                    firstName: firstName,
+                    lastName: lastName ?? '',
+                    salt: salt,
+                    password: hashedPassword
+                }
+            })
 
         } catch (error) {
             this.logger.error("Error while creating user!", error);
